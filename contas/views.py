35caaa725c76eed186166acf4131ffc762fa789d
Diego Lisboa <diego.lisboa.pires@hotmail.com>
2021-01-27 23:06:58 -0300

    Projeto DJango Fundamentos

--- contas/views.py
+++ contas/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contas.models import Transacao
 from django.http import HttpResponse
+from .forms import TransacaoForm
 import datetime
 
 # Create your views here.
 def home(request):
     return render(request, 'contas/home.html', {'nome' : 'Diego'})
     # return HttpResponse('<h1><strong>Contas - Controle Gastos</strong></h1>')
+
 def url(request):
     return HttpResponse(f'<h3>Teste {datetime.datetime.now()}</h3>')
 
 def listagem(request):
-    data = {}
-    data['transacoes'] = Transacao.objects.all() # Retornar todos os itens (objetos) para a lista
-    return render(request, 'contas/listagem.html', data)
+    transacoes = Transacao.objects.all() # Retornar todos os itens (objetos) para a lista
+    return render(request, 'contas/listagem.html', {'transacoes': transacoes})
+
+def nova_transacao(request):
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listagem')
+    return render(request, 'contas/form.html', {'form': form})
+
+def update(request, pk):
+    transacao = Transacao.objects.get(pk=pk) # filter() -> retornar mais de 1 objeto; get() -> retornar somente 1
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listagem')
+
+    return render(request, 'contas/form.html', {'form': form})
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    if transacao.delete():
+        return redirect('listagem')
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import url, home, listagem
+from contas.views import url, home, listagem, nova_transacao, update, delete
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
-    path('url/', url),
-    path('listagem/', listagem)
+    path('url/', url, name="url"),
+    path('listagem/', listagem, name="listagem"),
+    path('nova/', nova_transacao, name="nova"),
+    path('atualizar/<int:pk>/transacao/', update, name="update"),
+    path('deleta/<int:pk>/transacao/', delete, name="delete")
 ]
